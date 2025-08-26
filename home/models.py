e087a69f372a14f0f5ec8ec836b1d1b9cbbe2c73
Sarthak Kumar Seth <sarthakseth@email.com>
2025-08-27 01:12:36 +0530

    third commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    email=models.CharField(max_length=70)
+    reviews=models.TextField()
+
+def __str__(self):
+    return self.email
+
--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render,HttpResponse
+from home.models import Contact
 
 def index(request):
+    if request.method=="POST":
+        email=request.POST.get('email')
+        reviews=request.POST.get('reviews')
+        contact=Contact(email=email,reviews=reviews)
+        contact.save()
     return render(request,'index.html')
 def flavours(request):
     return render(request,'flavours.html')
